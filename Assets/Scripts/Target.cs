4c6d9695252f5a2612854b0339af94dcebdd006a
ZioulePang <50570732@qq.com>
2022-08-05 16:13:07 +0800

    Update

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,9 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     public GameObject GameController;
+    public GameObject CubeBlue, CubeYellow, CubeGreen, CubeRed;
+    //public event Action<int, int> UpdateHealthBar;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,35 @@ public class Target : MonoBehaviour
     {
         this.transform.Rotate(new Vector3(0, 5, 0));
     }
+
+
     private void OnTriggerEnter(Collider other)
     {
         //GameController.GetComponent<GameController>().LoseLife();
+        if (other.gameObject.CompareTag("BallBlue"))
+        {
+            takeDamage(CubeYellow, CubeRed, CubeGreen);
+        }else if (other.gameObject.CompareTag("BallYellow"))
+        {
+            takeDamage(CubeBlue, CubeRed, CubeGreen);
+        }
+        else if (other.gameObject.CompareTag("BallRed"))
+        {
+            takeDamage(CubeBlue, CubeYellow, CubeGreen);
+        }
+        else if (other.gameObject.CompareTag("BallGreen"))
+        {
+            takeDamage(CubeBlue, CubeRed, CubeYellow);
+        }
     }
+
+
+    public void takeDamage(GameObject one, GameObject two, GameObject three)
+    {
+        one.GetComponent<HealthBarUI>().Damage();
+        two.GetComponent<HealthBarUI>().Damage();
+        three.GetComponent<HealthBarUI>().Damage();
+    }
+
+
 }
